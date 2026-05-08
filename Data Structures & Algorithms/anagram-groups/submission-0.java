d2b6a6a0aaad8d530b38c0ebfb858118a96be580
Sankar K <sankar.work.email@gmail.com>
2026-05-08 07:01:04 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+
+
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for(String str:strs){
+            char[] charArray=str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = new String(charArray);
+            res.putIfAbsent(sortedS, new ArrayList<>());
+            res.get(sortedS).add(str);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
